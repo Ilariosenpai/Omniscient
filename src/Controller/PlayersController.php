5bf312abc7c61d4a0feb2bf80a1336725d2ca9f9
Ilariosenpai <ilanabougit2@gmail.com>
2024-07-03 16:56:30 +0200

    Oh death. Become my blade, once more.

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PlayersRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 class PlayersController extends AbstractController
 {
     #[Route('/players', name: 'app_players')]
-    public function index(): Response
+    public function index(PlayersRepository $playerRepository): Response
     {
+        $players = $playerRepository->findAll(); // Récupère tous les joueurs
+
         return $this->render('players/index.html.twig', [
-            'controller_name' => 'PlayersController',
+            'players' => $players, // Passe les joueurs à la vue
         ]);
     }
 }
