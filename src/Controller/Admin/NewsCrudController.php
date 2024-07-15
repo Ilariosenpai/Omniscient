45a26dbb569b0b9cf1e4121c347c680c5f00f022
Ilariosenpai <ilanabougit2@gmail.com>
2024-07-15 17:00:18 +0200

    rope + chair fast please 2

--- src/Controller/Admin/NewsCrudController.php
+++ src/Controller/Admin/NewsCrudController.php
@@ -4,10 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\News;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use PHPUnit\TextUI\XmlConfiguration\CodeCoverage\Report\Text;
 
 class NewsCrudController extends AbstractCrudController
 {
@@ -21,9 +23,12 @@ class NewsCrudController extends AbstractCrudController
     {
         return [
            
+
+            
             TextField::new('title'),
             DateField::new('date'),
             TextEditorField::new('content'),
+            AssociationField::new('image'),
             
 
         ];
--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\NewsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class NewsController extends AbstractController
 {
-    #[Route('/news', name: 'app_news')]
-    public function index(): Response
+    #[Route('/news', name: 'news_index')]
+    public function index(NewsRepository $newsRepository): Response
     {
+        $news = $newsRepository->findAll();
+
         return $this->render('news/index.html.twig', [
-            'controller_name' => 'NewsController',
+            'news' => $news,
+        ]);
+    }
+
+    #[Route('/news/{id}', name: 'news_show')]
+    public function show(int $id, NewsRepository $newsRepository): Response
+    {
+        $new = $newsRepository->find($id);
+
+        if (!$new) {
+            throw $this->createNotFoundException('The news does not exist');
+        }
+
+        return $this->render('news/show.html.twig', [
+            'new' => $new,
         ]);
     }
 }
